55fd0817085fffe84230b5490057aa6c2ee774d7
ecard58 <emanuelcardoso58@gmail.com>
2023-11-05 21:50:07 -0300

    envio final

--- src/pages/contato/Contato.jsx
+++ src/pages/contato/Contato.jsx
@@ -1,10 +1,24 @@
-import construcao from "../../components/img/construcao.png";
+import jblogo from "../../components/img/logo.png";
 
 function Contato() {
   return (
     <div>
       <h1>Contato</h1>
-      <img src={construcao} alt="aviso de página em construção" />
+      <img src={jblogo} alt="JamBand logo" width={200} />
+      <div>
+        <main>
+          <h3>Fale conosco através de:</h3>
+
+          <p>
+            Telefone: +55 24 2233-4455 <br />
+            <br />
+            Whatsapp: +55 24 999.888.777
+            <br />
+            <br />
+            E-mail: contato@jamband.com.br
+          </p>
+        </main>
+      </div>
     </div>
   );
 }
--- src/pages/postar/Postar.jsx
+++ src/pages/postar/Postar.jsx
@@ -1,12 +1,25 @@
-import construcao from "../../components/img/construcao.png";
+import jblogo from "../../components/img/logo.png";
+import upload from "../../components/img/upload.png";
 
 function Postar() {
   return (
     <div>
-      <div>
-        <h1>Postar</h1>
-        <img src={construcao} alt="aviso de página em construção" />
-      </div>
+      <img className="float-left" src={jblogo} alt="JamBand logo" width={50} />
+      <h1>Postar</h1>
+
+      <label>
+        <h2>Poste um vídeo com a sua JamBand:</h2>
+        <input
+          type="text"
+          placeholder="Digite aqui o nome da sua banda"
+          style={{ margin: 20, padding: 10, width: 255 }}
+        />
+      </label>
+      <br />
+      <img src={upload} />
+      <br />
+
+      <button>Postar</button>
     </div>
   );
 }
